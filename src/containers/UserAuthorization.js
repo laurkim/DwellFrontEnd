61f10048f3486e8f43b854b7501c72416f6a6421
laurkim <laurakeem@gmail.com>
2018-07-02 23:09:48 -0400

    Implement ability to register users and authorize existing users

--- src/containers/UserAuthorization.js
+++ src/containers/UserAuthorization.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class UserAuthorization extends Component {
 
-  createUser = event => {
+  registerUser = event => {
     event.preventDefault();
     let first_name = event.target.first_name.value
     let last_name = event.target.last_name.value
@@ -17,36 +17,88 @@ class UserAuthorization extends Component {
         "Accepts": "application/json"
       },
       body: JSON.stringify({
+        user: {
           first_name,
           last_name,
           username,
           password,
           password_confirmation,
           email
+        }
       })
     })
       .then(res => res.json())
-      .then(json => {debugger})
+      .then(json => {
+        debugger;
+        localStorage.setItem("token", json.token);
+        console.log("user has been created and local storage set");
+        console.log("token is", localStorage.getItem("token"));
+        console.log("------");
+      })
+  }
+
+  loginUser = event => {
+    event.preventDefault();
+    let username = event.target.username.value
+    let password = event.target.password.value
+    event.target.reset()
+    fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        debugger;
+        localStorage.setItem("token", json.token);
+        console.log("user has been authorized and local storage set");
+        console.log("token is", localStorage.getItem("token"));
+        console.log("------");
+      })
   }
 
   render() {
     return (
       <div>
         <p>user auth component</p>
-        <form onSubmit={this.createUser}>
+        <h2>User Registration</h2>
+        <form onSubmit={this.registerUser}>
           <label>First Name: </label>
           <input type="text" name="first_name" placeholder="First Name"/>
+          <br/>
           <label>Last Name: </label>
           <input type="text" name="last_name" placeholder="Last Name"/>
+          <br/>
           <label>Username: </label>
           <input type="text" name="username" placeholder="Username"/>
+          <br/>
           <label>Password: </label>
           <input type="password" name="password" placeholder="Password"/>
+          <br/>
           <label>Password Confirmation: </label>
           <input type="password" name="password_confirmation" placeholder="Password Confirmation"/>
+          <br/>
           <label>Email: </label>
           <input type="text" name="email" placeholder="Email"/>
+          <br/>
           <button type="submit">Create User</button>
+        </form>
+
+        <h2>User Login</h2>
+        <form onSubmit={this.loginUser}>
+          <label>Username: </label>
+          <input type="text" name="username" placeholder="Username"/>
+          <br/>
+          <label>Password: </label>
+          <input type="password" name="password" placeholder="Password"/>
+          <br/>
+          <button type="submit">Login User</button>
         </form>
       </div>
     )
