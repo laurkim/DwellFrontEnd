61c395557d9d14896278319b7b8913ba9a78f84a
laurkim <laurakeem@gmail.com>
2018-09-04 16:28:37 -0400

    Add in modal functionality for workspace

--- src/components/WorkspaceDetail.js
+++ src/components/WorkspaceDetail.js
@@ -6,22 +6,25 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
+// import CardActions from '@material-ui/core/CardActions';
+// import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import ShareIcon from '@material-ui/icons/Share';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
 class WorkspaceDetail extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      expanded: false
+      expanded: false,
+      open: false
     }
   }
 
@@ -37,6 +40,22 @@ class WorkspaceDetail extends Component {
     this.props.bookWorkspace(this.props.workspace.id, startTime, endTime);
   }
 
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  renderBookingForm = event => {
+    const { currentTarget } = event;
+    this.setState(({
+      anchorEl: currentTarget,
+      opener: !this.state.opener,
+    }));
+  }
+
   render() {
     const { name, image_url, yelp_url, rating, address_one, address_two, city, zip_code, latitude, longitude, phone } = this.props.workspace
     return (
@@ -48,13 +67,28 @@ class WorkspaceDetail extends Component {
               </Avatar>
             }
             action={
-              <IconButton>
+              <IconButton onClick={this.handleOpen}>
                 <MoreVertIcon />
               </IconButton>
             }
             title={name}
             subheader={phone}
           />
+          <Modal
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            open={this.state.open}
+            onClose={this.handleClose}
+            >
+            <div className="modal">
+              <Typography variant="title" id="modal-title">
+                Text in a modal
+              </Typography>
+              <Typography variant="subheading" id="simple-modal-description">
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </div>
+          </Modal>
           <CardMedia
             className="card-img"
             image={image_url}
@@ -62,38 +96,11 @@ class WorkspaceDetail extends Component {
           />
           <CardContent>
             <Typography component="p">
-              <p>Address: {address_one}</p>
-              <p>{city}, {zip_code}</p>
+                Address: {address_one}
+                <br />
+                {city}, {zip_code}
             </Typography>
           </CardContent>
-          <CardActions style={{display: 'flex'}} disableActionSpacing>
-            <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton>
-            <IconButton
-              onClick={this.handleExpandClick}
-              aria-expanded={this.state.expanded}
-              aria-label="Show more"
-              >
-              <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph variant="body2">
-                  Some Filler Text
-                </Typography>
-                <Typography paragraph>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eos illum, esse quod voluptatum? Nesciunt qui aperiam consequatur quasi eum, reiciendis impedit explicabo odit possimus soluta nam magnam quis excepturi.
-                </Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eos illum, esse quod voluptatum? Nesciunt qui aperiam consequatur quasi eum, reiciendis impedit explicabo odit possimus soluta nam magnam quis excepturi.
-                </Typography>
-              </CardContent>
-            </Collapse>
           </Card>
     )
   }
